9f05698f4bba5374545502a5b1eec1151bb8a703
Dan ODoherty <dodoherty339@gmail.com>
2012-10-17 21:08:13 -0500

    Added insert statements
    added some insert statements

--- DB SQL/Create Table.sql
+++ DB SQL/Create Table.sql
@@ -36,9 +36,28 @@ create table items
 	ForceVolume varchar(5),
 	PerUnitLabel varchar(5),
 	PerUnitAmount varchar(5),
-	LastPurchaseDate date,
-	LastPurchaseTime time,
+	LastPurchaseDateTime datetime,
 	BSTCashDefault varchar(5),
 	BSTCreditDefault varchar(5),
 	BSTAdded varchar(5),
 )
+
+insert into items
+	values('1234567','1','123456789','true','DVD','T','T','blank','1','5','N','N','8.00','15.00','15.00','5','4','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','False','Each','2011-01-01 02:30:45','0','0','0')
+	
+insert into items
+	values('1589674','1','864751293','true','Soft Drink','T','T','blank','2','5','N','N','.50','1.50','1.50','10','4','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','False','Each','2011-01-20 12:30:35','0','0','0')
+
+insert into items
+	values('7586942','1','468539712','true','DVD','T','T','blank','1','5','N','N','8.50','15.00','15.00','2','4','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','False','Each','2011-01-15 8:05:35','0','0','0')
+	
+insert into items
+	values('5896324','1','458796321','true','Poster','T','T','blank','1','6','N','N','5.00','10.00','10.00','9','4','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','False','Each','2011-02-03 13:01:25','0','0','0')	
+	
+insert into items
+	values('7856912','1','537914682','true','Keychain','T','T','blank','1','6','N','N','1.00','5.00','5.00','4','4','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','False','Each','2011-02-10 16:15:45','0','0','0')	
+	
+
+	
+	
+	
