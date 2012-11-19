15121a2450068fd380d0dc992d959dd45fa69d2f
Dan ODoherty <dodoherty339@gmail.com>
2012-11-18 21:33:23 -0600

    Created table manually
    Added the manual table creation and commented out the regular table
    initialization

--- Amazon_Price_Finder/Amazon_Price_Finder/Program.cs
+++ Amazon_Price_Finder/Amazon_Price_Finder/Program.cs
@@ -30,7 +30,37 @@ namespace Amazon_Price_Finder
         {
             double price;
             AmazonPriceFinderForm.FormAmazonPrice form = new AmazonPriceFinderForm.FormAmazonPrice();
-            DataTable set = Database_Conn.getDBResults();
+            //regular initialization
+            //DataTable set = Database_Conn.getDBResults();
+
+            //new initialization start
+            DataTable set = new DataTable();
+            DataRow setRow = set.NewRow();
+            DataRow setRow1 = set.NewRow();
+            DataRow setRow2 = set.NewRow();
+            DataRow setRow3 = set.NewRow();
+            set.Columns.Add("IDNumber", typeof(string));
+            set.Columns.Add("ItemDesc", typeof(string));
+            set.Columns.Add("Retail", typeof(double));
+
+            setRow["IDNumber"] = "008888526841";
+            setRow["ItemDesc"] = "Assassins Creed Revelations";
+            setRow["Retail"] = "28.00";
+            set.Rows.Add(setRow);
+            setRow1["IDNumber"] = "047875842069";
+            setRow1["ItemDesc"] = "Call Of Duty MW3";
+            setRow1["Retail"] = "8.61";
+            set.Rows.Add(setRow1);
+            setRow2["IDNumber"] = "085391117018";
+            setRow2["ItemDesc"] = "V for Vendetta";
+            setRow2["Retail"] = "8.25";
+            set.Rows.Add(setRow2);
+            setRow3["IDNumber"] = "024543563969";
+            setRow3["ItemDesc"] = "Office Space";
+            setRow3["Retail"] = "8.61";
+            set.Rows.Add(setRow3);
+            //END OF MANUAL ADD
+
             set.Columns.Add("GooglePrice", typeof(double));
 
             foreach (DataRow row in set.Rows)
