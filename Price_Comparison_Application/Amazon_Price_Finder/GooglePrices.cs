c61800ccd81b878495dc29befa2004c996c61602
Dan ODoherty <dodoherty339@gmail.com>
2012-12-06 11:38:46 -0600

    oops

--- Price_Comparison_Application/Amazon_Price_Finder/GooglePrices.cs
+++ Price_Comparison_Application/Amazon_Price_Finder/GooglePrices.cs
@@ -30,23 +30,23 @@ namespace Price_Comparison
          * them in an array called totals. Totals is sent to AnalyzeResults in 
          * which we get a double in return. getPrice then returns that double.
          */
-        public static double getPrice(string barcode)
+        public static double getPrice(string barcode, string dscr)
         {
             // There is a 2500 per day limit on the searches, so for testing, we will probably need more than one key
-            //string key = "AIzaSyAkuzeoL2MJz--Gk2WSGnotf2qGoCaZdL8";
-            //string key = "AIzaSyDCT6BVSo8d8zpW8cZJ65LodutNTs_00Lo"; //Lucy's gmail
-            string key = "AIzaSyBsYY7PHtb-EH0VO2S_f9QalhfyXq8K4X8"; //Lucy's gmav
+            //string key = "AIzaSyAkuzeoL2MJz--Gk2WSGnotf2qGoCaZdL8"; //Dan's gmail
+            string key = "AIzaSyDCT6BVSo8d8zpW8cZJ65LodutNTs_00Lo"; //Lucy's gmail
+            //string key = "AIzaSyBsYY7PHtb-EH0VO2S_f9QalhfyXq8K4X8"; //Lucy's gmav
             //string barcode = "008888526841";
-            string sourceCode = getSourceCode("https://www.googleapis.com/shopping/search/v1/public/products?key=" + key + "&country=US&q=" + barcode + "&alt=JSON");
+            string sourceCode = getSourceCode("https://www.googleapis.com/shopping/search/v1/public/products?key=" + key + "&country=US&q=" + barcode + "|" + dscr + "&alt=JSON");
             string subPrice;
-            Regex name = new Regex("\"name\": \".*\"");
-            Regex condition = new Regex("\"condition\": \".*\"");
+            //Regex name = new Regex("\"name\": \".*\"");
+            //Regex condition = new Regex("\"condition\": \".*\"");
             Regex price = new Regex("\"price\": \\d+[.]\\d+");
-            Regex shipping = new Regex("\"shipping\": \\d+[.]\\d+,");
-            MatchCollection mcn = name.Matches(sourceCode);
-            MatchCollection mcc = condition.Matches(sourceCode);
+            //Regex shipping = new Regex("\"shipping\": \\d+[.]\\d+");
+            //MatchCollection mcn = name.Matches(sourceCode);
+            //MatchCollection mcc = condition.Matches(sourceCode);
             MatchCollection mcp = price.Matches(sourceCode);
-            MatchCollection mcs = shipping.Matches(sourceCode);
+            //MatchCollection mcs = shipping.Matches(sourceCode);
             double[] totals = new double[mcp.Count];
 
             for (int i = 0; i < mcp.Count; ++i)
