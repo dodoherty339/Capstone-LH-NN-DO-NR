e991d377e055cf133716d7538bb88b7502153792
Dan <dodoherty339@gmail.com>
2012-12-11 07:24:18 -0600

    changed keys

--- Price_Comparison_Application/Amazon_Price_Finder/GooglePrices.cs
+++ Price_Comparison_Application/Amazon_Price_Finder/GooglePrices.cs
@@ -34,9 +34,9 @@ namespace Price_Comparison
         {
             // There is a 2500 per day limit on the searches, so for testing, we will probably need more than one key
             //string key = "AIzaSyAkuzeoL2MJz--Gk2WSGnotf2qGoCaZdL8"; //Dan's gmail
-            string key = "AIzaSyDCT6BVSo8d8zpW8cZJ65LodutNTs_00Lo"; //Lucy's gmail
+            //string key = "AIzaSyDCT6BVSo8d8zpW8cZJ65LodutNTs_00Lo"; //Lucy's gmail
             //string key = "AIzaSyBsYY7PHtb-EH0VO2S_f9QalhfyXq8K4X8"; //Lucy's gmav
-            //string key = "AIzaSyAVsFc9Sv94UMMQFAtk-uA8Bp0AogfiJ1A"; //Nick's GMav
+            string key = "AIzaSyAVsFc9Sv94UMMQFAtk-uA8Bp0AogfiJ1A"; //Nick's GMav
             //string barcode = "008888526841";
             string sourceCode = getSourceCode("https://www.googleapis.com/shopping/search/v1/public/products?key=" + key + "&country=US&q=" + barcode + "&alt=JSON");
             //string sourceCode = getSourceCode("https://www.googleapis.com/shopping/search/v1/public/products?key=" + key + "&country=US&q=" + barcode + "|" + dscr + "&alt=JSON");
